20a57d4996c84a4b4257ce616fed3c05d754849c
Yuno <36519646+Axiks@users.noreply.github.com>
2024-10-05 19:48:42 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import "@radix-ui/themes/styles.css";
+import * as Form from "@radix-ui/react-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { Theme } from "@radix-ui/themes";
+
+export default function () {
+	return (
+		<html>
+			<body>
+				<Theme>
+          <FormDemo />
+				</Theme>
+			</body>
+		</html>
+	);
 }
 
-export default App;
+const FormDemo = () => (
+	<Form.Root className="FormRoot">
+
+		<Form.Field className="FormField" name="email">
+			<div
+				style={{
+					display: "flex",
+					alignItems: "baseline",
+					justifyContent: "space-between",
+				}}
+			>
+				<Form.Label className="FormLabel">Email</Form.Label>
+				<Form.Message className="FormMessage" match="valueMissing">
+					Please enter your email
+				</Form.Message>
+				<Form.Message className="FormMessage" match="typeMismatch">
+					Please provide a valid email
+				</Form.Message>
+			</div>
+			<Form.Control asChild>
+				<input className="Input" type="email" required />
+			</Form.Control>
+		</Form.Field>
+
+		<Form.Field className="FormField" name="password">
+			<div
+				style={{
+					display: "flex",
+					alignItems: "baseline",
+					justifyContent: "space-between",
+				}}
+			>
+				<Form.Label className="FormLabel">Password</Form.Label>
+				<Form.Message className="FormMessage" match="valueMissing">
+					Please enter password
+				</Form.Message>
+			</div>
+			<Form.Control asChild>
+				<input className="Input" type="password" required />
+			</Form.Control>
+		</Form.Field>
+		<Form.Submit asChild>
+			<button className="Button" style={{ marginTop: 10 }}>
+				Login
+			</button>
+		</Form.Submit>
+	</Form.Root>
+);
